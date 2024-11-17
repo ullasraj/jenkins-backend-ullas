7041771964f2f2f7b30c4bd3eef21a0171cc1dbf
ullasraj <ullas@exprzn.com>
2024-11-17 12:18:45 +0530

    added webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,36 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './index.ts',  // Entry point of your TypeScript application
-  output: {
-    filename: 'bundle.js',  // The bundled JavaScript file output
-    path: path.resolve(__dirname, 'dist'),  // Output directory for bundled files
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],  // Resolve both TypeScript and JavaScript files
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,             // Process files with .ts extension
-        use: 'ts-loader',          // Use ts-loader to compile TypeScript files
-        exclude: /node_modules/,   // Don't process files in node_modules
-      },
-    ],
-  },
-  mode: 'production',            // Set Webpack mode to 'production' for optimization
+	// watch: true,
+	entry: "./index.ts",
+	//Important - we ar building for node server
+	mode: "development",
+	target: "node",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+            {
+                test: /\.d\.ts$/,
+                use: 'null-loader',
+            },
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "dist"),
+		library: { name: "buyers", type: "umd" },
+		// library: 'dgraph_orm',
+		// libraryTarget:'umd',
+		// umdNamedDefine: true
+	},
+	optimization: {
+		minimize: false,
+	},
 };
